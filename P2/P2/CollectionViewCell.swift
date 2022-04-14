b693dad33e29d8efb59900c854223b115bdff1e4
zahraaherz <61514777+zahraaherz@users.noreply.github.com>
2022-04-14 13:28:05 +0300

    Add files via upload

---
+++ P2/P2/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  P2
+//
+//  Created by Zahraa Herz on 13/04/2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var Name: UILabel!
+    @IBOutlet var Images: UIImageView!
+    
+}
